d7bc5e9bf6a41ce7932bc2221b5b1aed2b7ffc10
hariharancse <princehari.selvaraj@gmail.com>
2024-11-25 17:58:15 +0530

    Terraform OCI - Compute
    Make sure add the required detail like Tenancy OCID , Image OCID, Subnet OCID, Private key path, Fingerprint,..

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "name-of-first-availability-domain" {
+  value = data.oci_identity_availability_domains.ads.availability_domains[0].name
+}
+
+
+
+# Outputs for compute instance
+
+output "public-ip-for-compute-instance" {
+  value = oci_core_instance.ubuntu_instance.public_ip
+}
---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci"{
+tenancy_ocid="<Tenancy ID>"
+user_ocid="<User OCID>"
+private_key_path="<Path of your Key file local>"
+fingerprint="<Fingerprint API key>"
+region="ap-mumbai-1"
+}
